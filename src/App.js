7c374db65418f4e3e10455c0a2b173e16ead0735
Swathi Paipalle <swathi.paipalle.gmail.com>
2023-02-28 23:22:49 -0800

    Add show/hide

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [show, setShow] = React.useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Hide Element Below" : "Show Element Below"}
+      </button>
+
+      {show && <div>Toggle Challenge</div>}
+    </>
   );
 }
 
